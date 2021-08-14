8586e6fca48557adc70365e4a59d5187d813703f
Elmer <65182186+elmersan@users.noreply.github.com>
2021-08-14 18:00:34 -0500

    update-date

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".number").countdown("2020/12/24 24:00:00", function (event) {
+  $(".number").countdown("2021/08/15 18:00:00", function (event) {
     $("#days").html(event.strftime("%D"));
     $("#hours").html(event.strftime("%H"));
     $("#minutes").html(event.strftime("%M"));
